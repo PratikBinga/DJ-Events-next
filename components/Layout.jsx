7d7dd7b09aa14d36607c0ef7c930e54d3c08828e
PratikBinga <pratik.swvk@gmail.com>
2021-11-25 19:32:33 +0530

    Displaying event items from local

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styles from "../styles/Layout.module.css";
+import Showcase from "./Showcase";
+import { useRouter } from "next/router";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head title={title}>
@@ -11,6 +14,7 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
       </Head>
       <Header />
+      {(router.pathname === "/" && <Showcase />)}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,28 @@
-export default function index() {
-  return <div>Event Page</div>
+import Layout from "../../components/Layout";
+import Link from "next/link";
+import EventItem from "../../components/EventItem";
+
+export default function index({ result }) {
+  console.log(result, "data");
+  return (
+    <Layout>
+      <Link href="/">
+        <a> Home</a>
+      </Link>
+      {result?.map((evt, idx) => (
+        <EventItem evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  const data = await fetch("http://localhost:3000/api");
+  const result = await data.json();
+  console.log(result, "result");
+  return {
+    props: {
+      result,
+    },
+  };
 }
